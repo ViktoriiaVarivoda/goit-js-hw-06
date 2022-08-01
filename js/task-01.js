2e8642fb576766a62d536f90c5bbf295d91937f5
ViktoriiaVarivoda <victusia87@gmail.com>
2022-08-02 00:17:59 +0300

    exercise 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,13 @@
-const item = document.querySelector('.item');
-console.log(item);
+const categories = document.querySelector('#categories');
+const children = categories.children;
+const amount = children.length;
+console.log("Number of categories:", amount);
+
+const item = Array.from(children);
+for (let elem of item) {
+    let category = elem.firstElementChild.textContent;
+    let quantityElem = elem.lastElementChild.children.length;
+    console.log(
+    `Category: ${category}
+Elements: ${quantityElem} `);
+}
