634eb96688eb1f98c7c0891378028e48f0a0aba3
ViktoriiaVarivoda <victusia87@gmail.com>
2022-08-07 16:55:49 +0300

    exercise 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const totalCategories = document.querySelector("#ingredients");
+
+const elements = ingredients.map(ingredient => {
+const elementLi = document.createElement('li');
+
+ elementLi.textContent =(ingredient); 
+ elementLi.classList.add('item');     
+  return elementLi;
+});
+
+totalCategories.append(...elements);
+
+console.log(totalCategories);
