6f0464fc6d5b05745120969864cf22b09b3fcea9
ViktoriiaVarivoda <victusia87@gmail.com>
2022-08-08 20:35:33 +0300

    exercise 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColor = document.querySelector('.change-color')
+const colorValue = document.querySelector('span.color')
+const body = document.querySelector('body')
+
+changeColor.addEventListener('click', () => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
+})
